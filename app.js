c29de8d5157c034db9d598ba75eda73657f9c6d9
Joseph Yølk Chiocchi <joe@yolk.cc>
2013-05-22 17:20:09 -0500

    working again :3

--- app.js
+++ app.js
@@ -18,6 +18,7 @@ params.extend(app);
 var prod = app.settings.prod;// = /^production$/i.test(app.settings.env);
 console.log('running in %j mode (prod: %s)', app.settings.env, prod);
 
+var root_path = app.settings.root_path = __dirname;
 
 /**
  * Connect logger.
@@ -36,20 +37,20 @@ if (prod) {
 
 
 // `req.article_names` is used by every request
-app.get('*', require('./lib/article-names'));
+// app.get('*', require('./lib/article-names'));
 
 app.get('*', require('./lib/work-names'));
 // by now `req.root_tree` is a "git_tree" instance to the resolved SHA
 app.get('/', require('./lib/homepage'));
-app.get('/articles', require('./lib/articles'));
+// app.get('/articles', require('./lib/articles'));
 
 // redirect blog articles to have a trailing "/" (this is necessary because of
 // the way the browser serves files from relative URLs)
 //app.get('*', require('./lib/article-redirect'));
 
-app.param('article', /^[0-9a-z-_]+$/);
+// app.param('article', /^[0-9a-z-_]+$/);
 // serve static article/ items (defined by post_name with corresponding stiatic dir)
-app.get('/article/:article', require('./lib/article'));
+// app.get('/article/:article', require('./lib/article'));
 
 // attempt to render an article if this a request for one
 //app.get('*', require('./lib/article'));
