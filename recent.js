ffb94135188c43544e9c6e11ff56327e119465f6
Joseph Yølk Chiocchi <joe@yolk.cc>
2013-10-13 23:41:06 -0500

    recent.js revert

--- recent.js
+++ recent.js
@@ -1,7 +1,7 @@
 var mdw = require('markdown-walker');
 var path = require('path');
 
-var walker = mdw('works/', {lastModified: new Date((new Date).getFullYear(), (new Date).getMonth()-3)});
+var walker = mdw('works/', {lastModified: new Date((new Date).getFullYear(), (new Date).getMonth()-2)});
 files = [];
 walker.on('modified', function(file, stat) {
   // files.push(''+file);
