7a1dfa9011865a54587641034ea494558b231496
Joseph Yølk Chiocchi <joe@yolk.cc>
2014-01-25 14:32:33 -0600

    update paths

--- config.rb
+++ config.rb
@@ -8,7 +8,7 @@ http_path = "/"
 css_dir = "/public/css"
 sass_dir = "/sass"
 images_dir = "/public/img"
-fonts_dir = "/public/css/fonts"
+fonts_dir = "/public/fonts"
 javascripts_dir = "/public/js"
 
 # You can select your preferred output style here (can be overridden via the command line):
